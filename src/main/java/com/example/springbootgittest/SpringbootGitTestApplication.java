4f9922d2dd200ebd7eb9ec045ac263f01f6b53e7
minbo2002 <minbo2002@naver.com>
2022-08-25 10:38:09 +0900

    first commit

---
+++ src/main/java/com/example/springbootgittest/SpringbootGitTestApplication.java
@@ -0,0 +1,14 @@
+package com.example.springbootgittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGitTestApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringbootGitTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootgittest/SpringbootGitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootgittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGitTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
